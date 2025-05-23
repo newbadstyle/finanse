95ef64fdcf153e07a4705cfacc9dea4af5792ef7
newbadstyle <newbadstyle@gmail.com>
2025-05-23 13:40:44 +0200

    Refactor WelcomePage class and implement initial UI layout

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'register_page.dart';
 
-class name extends StatelessWidget {
-  const name({super.key});
-
+class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('Witaj')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Kontroluj swoje finanse już dziś',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+              child: Text('Logowanie'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegisterPage()),
+                );
+              },
+              child: Text('Rejestracja'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
