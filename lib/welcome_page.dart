46fe2954e170b09111db7173e4c7bc4d3510ed8b
newbadstyle <newbadstyle@gmail.com>
2025-05-27 16:50:43 +0200

    feat: Update WelcomePage layout and improve UI elements

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -7,45 +7,55 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'NINJA',
-              style: TextStyle(
-                fontFamily: 'JosefinSans',
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 50),
+              Text(
+                'NINJA',
+                style: TextStyle(
+                  fontFamily: 'JosefinSans',
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Lottie.asset('assets/lotties/hello.json'),
-            Text(
-              'Witaj!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            Text('Twój osobisty przewodnik po finansach Ninja'),
-            SizedBox(height: 20),
-            FilledButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
-              child: Text('Logowanie'),
-            ),
-            SizedBox(height: 10),
-            FilledButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RegisterPage()),
-                );
-              },
-              child: Text('Rejestracja'),
-            ),
-          ],
+              Lottie.asset(
+                'assets/lotties/hello.json',
+                width: 600,
+                height: 450,
+              ),
+              Text(
+                'Witaj w Ninja!',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              Text('Twój osobisty przewodnik po finansach'),
+              SizedBox(height: 15),
+              FilledButton(
+                style: FilledButton.styleFrom(minimumSize: Size(300, 40)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+                child: Text('Logowanie'),
+              ),
+              SizedBox(height: 5),
+              FilledButton(
+                style: FilledButton.styleFrom(minimumSize: Size(300, 40)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RegisterPage()),
+                  );
+                },
+                child: Text('Rejestracja'),
+              ),
+              SizedBox(height: 50),
+            ],
+          ),
         ),
       ),
     );
